8311495642f3206a5abbffa6bc1ad08ad55e6a8d
Geisy Domiciano <geisydomiciano@gmail.com>
2017-09-03 10:58:30 -0300

    add google maps autocomplete

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue';
 import VueApollo from 'vue-apollo';
 import ApolloClient, { createNetworkInterface } from 'apollo-client';
 // import ApolloClient, { createNetworkInterface, addTypename } from 'apollo-client';
+import * as VueMaps from 'vue2-google-maps';
 import { router } from './routes';
 import App from './App.vue';
 
@@ -14,8 +15,16 @@ const apolloClient = new ApolloClient({
     // queryTransformer: addTypename,
     dataIdFromObject: r => r.id,
 });
+const mapsKey = 'AIzaSyBK6YopQF4b5TuNUIzlCfqL7QaiI5q7lto';
 
 Vue.use(VueApollo, { apolloClient });
+Vue.use(VueMaps, {
+    load: {
+        key: mapsKey,
+        libraries: 'places',
+        // libraries: 'places,drawing,visualization',
+    },
+});
 
 // eslint-disable-next-line
 new Vue({
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,13 +1,58 @@
 <template>
     <div>
-         <router-link to="/results">huhuhu</router-link>
-
+        <router-link to="/results">huhuhu</router-link>
+        <div class="autocomplete">
+            <label>
+            AutoComplete
+            <gmap-autocomplete @place_changed="setPlace"></gmap-autocomplete>
+            <button @click="usePlace">Add</button>
+            </label>
+            <br/>
+            <Gmap-Map style="width: 600px; height: 300px;" :zoom="1" :center="{lat: 0, lng: 0}">
+                <Gmap-Marker v-for="(marker, index) in markers" :key="index" :position="marker.position" ></Gmap-Marker>
+                <Gmap-Marker v-if="this.place" label="&#x2605;" :position="{ lat: this.place.geometry.location.lat(), lng: this.place.geometry.location.lng(),}"></Gmap-Marker>
+            </Gmap-Map>
+            <!-- {{latLng.lat}},
+            {{latLng.lng}} -->
+            @-23.6315238,-46.7040137
+        </div>
     </div>
 </template>
 
 <script>
-    export default {
+    // import gql from 'graphql-tag';
 
+    // const addressQuery = gql `
+    //     query pocSearch (algorithm: 'NEAREST', lat: '', lon: '', now:new Date()){
+    //         id
+    //     }
+    // `;
+    export default {
+        data() {
+            return {
+                markers: [],
+                place: null,
+            };
+        },
+        methods: {
+            setDescription(description) {
+                this.description = description;
+            },
+            setPlace(place) {
+                this.place = place;
+            },
+            usePlace() {
+                if (this.place) {
+                    this.markers.push({
+                        position: {
+                            lat: this.place.geometry.location.lat(),
+                            lng: this.place.geometry.location.lng(),
+                        },
+                    });
+                    this.place = null;
+                }
+            },
+        },
     };
 </script>
 
