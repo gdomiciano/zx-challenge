1f13d6d11f7062820660b90402c895b2455917ea
Geisy Domiciano <geisydomiciano@gmail.com>
2017-09-07 10:02:32 -0300

    test deploy

---
+++ src/components/bg-map.vue
@@ -0,0 +1,41 @@
+<template>
+    <Gmap-Map class="map" style="width: mapSize.width ; height: mapSize.height;" :width="mapSize.width" :height="mapSize.height" :zoom="zoom" :center="position" :options='options'>
+        <Gmap-Marker v-for="(marker, index) in markers" :key="index" :position="marker.position"></Gmap-Marker>
+        <Gmap-Marker v-if="place" label="You" :position="position"></Gmap-Marker>
+    </Gmap-Map>
+</template>
+
+<script>
+    export default {
+        name: 'bg-map',
+        props: ['place', 'zoom', 'markers', 'position'],
+        data() {
+            return {
+                mapSize: {
+                    width: `${window.innerWidth}px`,
+                    height: window.innerHeight,
+                },
+                options: {
+                    fullscreenControl: false,
+                    mapTypeControl: false,
+                    streetViewControl: false,
+                    zoomControl: false,
+                    scrollwheel: false,
+                },
+            };
+        },
+    };
+</script>
+
+<style scoped lang="scss">
+
+    .map{
+        height: 0;
+        padding: 100%;
+        position: relative;
+        overflow: hidden;
+        right: 50%;
+        top: -30%;
+    }
+
+</style>
---
+++ src/components/welcome.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="welcome">
+        <h2 class="page-title">Cheers!</h2>
+        <p class="page-subtitle">It's time to get your beer delivered ;)</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'welcome',
+    };
+</script>
+
+<style scoped lang="scss">
+    @import '../assets/scss/colors';
+
+    .welcome{
+        text-align: center;
+        margin-bottom: 30px;
+    }
+    .page-title {
+        color: color($orange, 500);
+        font-size: 60px;
+    }
+    .page-subtitle {
+        color: color($white);
+        font-size: 30px;
+
+    }
+</style>
